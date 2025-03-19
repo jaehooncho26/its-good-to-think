fb59b89b980cb24c0f3fc93a8156209a894a291a
jaehooncho26 <jaehooncho26@gmail.com>
2025-03-19 15:54:42 -0700

    feat: updating and improving the questionspage layout.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -28,4 +28,24 @@ window.onload = function() {
     q1Button.addEventListener("click", function() {
         window.location.href = "q1-page.html";
     });
+	
+	const q2Button = document.getElementById("q2-button");
+	q2Button.addEventListener("click", function() {
+	    window.location.href = "q2-page.html";
+	});
+	
+	const q3Button = document.getElementById("q3-button");
+	q3Button.addEventListener("click", function() {
+	    window.location.href = "q3-page.html";
+	});
+	
+	const q4Button = document.getElementById("q4-button");
+	q4Button.addEventListener("click", function() {
+	    window.location.href = "q4-page.html";
+	});
+	
+	const q5Button = document.getElementById("q5-button");
+	q5Button.addEventListener("click", function() {
+	    window.location.href = "q5-page.html";
+	});
 };
